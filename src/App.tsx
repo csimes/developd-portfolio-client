be603983a6938da47397a45058cdb717535cd63f
Cherron Simes <cherron@developdbycherron.com>
2022-06-29 21:45:07 -0500

    Split intro and about into separate components, updated hero and nav styling for smooth transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import "./App.css";
+import About from "./components/home/About";
 import Intro from "./components/home/Intro";
 import Nav from "./components/nav/Nav";
 import ProjectIndex from "./components/projects/ProjectIndex";
-import logo from "./assets/transparent.png";
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       <Nav />
-      <div className="hero">
-        <div className="intro">
-          <img className="logo" src={logo} alt="" />
-          <h1>Hey, I'm </h1>
-          <h1 className="name">Cherron!</h1>
-          <h1>Web Developer and owner of</h1>{" "}
-          <h1 className="name">developd.</h1>
-        </div>
-      </div>
       <Intro />
+      <About />
       <ProjectIndex />
-    </>
+    </div>
   );
 };
 
