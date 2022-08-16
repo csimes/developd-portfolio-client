ac70bd6b63f795f63eaff9a7566823ac408494e4
Cherron Simes <cherron@developdbycherron.com>
2022-08-15 20:34:28 -0500

    Moved <code>Head</code> to layout component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,18 @@
+import { BottomNavigation } from "@mui/material";
 import Link from "next/link";
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      programmed to perfection by&nbsp;
-      <Link href="/">
-        <a>developd</a>
-      </Link>
-      &nbsp; &copy; 2022
-    </footer>
+    <BottomNavigation className={styles.footer}>
+      <p>
+        programmed to perfection by&nbsp;{" "}
+        <Link href="/">
+          <a>developd</a>
+        </Link>
+        &nbsp; &copy; 2022
+      </p>
+    </BottomNavigation>
   );
 };
 
