773305c9dd9525d75096e23a7d1481895a401676
Cherron Simes <cherron@developdbycherron.com>
2022-08-24 21:12:05 -0500

    Added fontsource package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/globals.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
 
 function MyApp({ Component, pageProps }: any) {
   return (
