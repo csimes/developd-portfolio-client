12572bf7a7eac5de6beca669cf27c6252022f534
Cherron Simes <cherron@developdbycherron.com>
2022-09-08 21:42:27 -0500

    Removed MUI components from footer to resolve styling not working in production

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,17 @@
-import { BottomNavigation, Paper } from "@mui/material";
 import Link from "next/link";
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-      <BottomNavigation className={styles.footer}>
-        <p>
-          programmed to perfection by&nbsp;{" "}
-          <Link href="/">
-            <a>developd</a>
-          </Link>
-          &nbsp; &copy; 2022
-        </p>
-      </BottomNavigation>
-    </Paper>
+    <div className={styles.footer}>
+      <p>
+        programmed to perfection by&nbsp;{" "}
+        <Link href="/">
+          <a>developd</a>
+        </Link>
+        &nbsp; &copy; 2022
+      </p>
+    </div>
   );
 };
 
