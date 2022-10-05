8a22e1882cdfbed7d0061188809737b035082e56
Cherron Simes <cherron@developdbycherron.com>
2022-10-04 19:09:20 -0500

    Add Sass & (React) Bootstrap, convert global css to scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
-import "../styles/globals.css";
+import "../styles/globals.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/700.css";
