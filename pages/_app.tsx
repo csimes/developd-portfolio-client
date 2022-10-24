4a5b2562eb5af08378d5dafe0f3ec05d6c9af6a3
Cherron Simes <cherron@developdbycherron.com>
2022-10-23 19:57:49 -0500

    Add font to head, remove font package & imports from app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import Layout from "../components/layout";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fontsource/montserrat/400.css";
-import "@fontsource/montserrat/500.css";
-import "@fontsource/montserrat/700.css";
 
 function MyApp({ Component, pageProps }: any) {
   return (
