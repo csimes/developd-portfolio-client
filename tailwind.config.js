5b91e12597c8b061e85d6c9340a0718164abc17a
Cherron Simes <cherron@developdbycherron.com>
2023-07-12 21:30:44 -0500

    add brand colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
+    colors: {
+      "medium-orange": "#e79d63",
+      "dark-gray": "#47463b",
+      "light-gray": "#807e71",
+      "soft-white": "#eae7d4",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
