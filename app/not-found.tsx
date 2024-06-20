47f8f5d9832dfb9ad5cab01df982f2016fe0a417
csimes <cherron@developdbycherron.com>
2024-06-19 23:50:33 -0500

    add custom 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,47 @@
 "use client";
 import { Link } from "@chakra-ui/next-js";
+import { Box, Flex, Heading, Text, Button, Image } from "@chakra-ui/react";
 import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="text-center">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource!</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <Flex
+      maxH="100vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="row"
+    >
+      <Box textAlign="center">
+        <Heading
+          mb={4}
+          className="color-medium-orange"
+        >
+          Uh oh..
+        </Heading>
+        <Heading
+          mb={6}
+          size="lg"
+          fontWeight="normal"
+        >
+          Something went wrong
+        </Heading>
+        <Text mb={8}>Looks like this page doesn't exist or was removed.</Text>
+        <Button
+          size="lg"
+          as="a"
+          href="/"
+          className="bg-medium-orange transition duration-300 hover:scale-105 transform"
+        >
+          Back to Home
+        </Button>
+      </Box>
+      <Box mt={12}>
+        <Image
+          boxSize="50vw"
+          src="/assets/404.png"
+          alt="404 Illustration"
+        />
+      </Box>
+    </Flex>
   );
 }
