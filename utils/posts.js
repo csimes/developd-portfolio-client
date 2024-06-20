808b9b7aa4dad9a81a8015b0466907a59ea7bd03
csimes <cherron@developdbycherron.com>
2024-06-19 23:51:30 -0500

    set up gray matter to read file data

---
+++ utils/posts.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+import matter from "gray-matter";
+
+const getPostMetadata = (basePath) => {
+  const postDirectory = basePath + "/";
+  const files = fs.readdirSync(postDirectory);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+
+  // get the file data
+  const posts = markdownPosts.map((filename) => {
+    const fileContents = fs.readFileSync(`${basePath}/${filename}`, "utf8");
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      slug: filename.replace(".md", ""),
+    };
+  });
+  return posts;
+};
+
+export default getPostMetadata;
